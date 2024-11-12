25c383145df74ea33e5268886da7846a33ea9eb9
unknown <brightbassey1819@gmail.com>
2024-11-12 12:38:10 +0000

    fixed hero section font size

--- components/ShadowTemplate/nav.tsx
+++ components/ShadowTemplate/nav.tsx
@@ -1,47 +1,27 @@
 "use client";
 import Link from "next/link";
 
+const navItems = [
+  { href: "#about", label: "ABOUT" },
+  { href: "#experience", label: "EXPERIENCE" },
+  { href: "#skills", label: "SKILLS" },
+  { href: "#projects", label: "PROJECTS" },
+];
+
 export function ShadowNav() {
   return (
     <nav className="fixed left-[40px] top-1/2 -translate-y-1/2 hidden lg:block">
       <ul className="flex flex-col space-y-24">
-        <li>
-          <Link 
-            href="#about" 
-            className="text-black font-mono text-[13px] tracking-widest opacity-70 hover:opacity-100 transition-opacity"
-            style={{ writingMode: 'vertical-rl' }}
-          >
-            ABOUT
-          </Link>
-        </li>
-        <li>
-          <Link 
-            href="#experience" 
-            className="text-black font-mono text-[13px] tracking-widest opacity-70 hover:opacity-100 transition-opacity"
-            style={{ writingMode: 'vertical-rl' }}
-          >
-            EXPERIENCE
-          </Link>
-        </li>
-        <li>
-          <Link 
-            href="#skills" 
-            className="text-black font-mono text-[13px] tracking-widest opacity-70 hover:opacity-100 transition-opacity"
-            style={{ writingMode: 'vertical-rl' }}
-          >
-            SKILLS
-          </Link>
-        </li>
-        <li>
-          <Link 
-            href="#projects" 
-            className="text-black font-mono text-[13px] tracking-widest opacity-70 hover:opacity-100 transition-opacity"
-            style={{ writingMode: 'vertical-rl' }}
-          >
-            PROJECTS
-          </Link>
-        </li>
+        {navItems.map((item) => (
+          <li key={item.href}>
+            <Link 
+              href={item.href}
+              className="text-black font-mono text-[13px] tracking-widest opacity-70 hover:opacity-100 transition-opacity"
+              style={{ writingMode: 'vertical-rl' }}
+            >
+              {item.label}
+            </Link>
+          </li>
+        ))}
       </ul>
-    </nav>
-  );
-}
+      </nav>)}
